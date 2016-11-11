c4ce1f67adb303aa9aecaa4a071c8464c34733f7
Abimbola Adegun <abimbola.adegun@gmail.com>
2016-11-11 16:04:12 +0100

    Verify function added, not yet tested. Function spec included.

--- src/erlpaystack.erl
+++ src/erlpaystack.erl
@@ -1,15 +1,37 @@
 -module (erlpaystack).
 
--export ([initialize/4]).
+-export ([initialize/4, verify/2]).
+-define (InitUrl, "https://api.paystack.co/transaction/initialize").
+-define (VerifyUrl, "https://api.paystack.co/transaction/verify").
+
+-spec initialize(SecretKey::string(), Reference::string(), Amount::string(), Email::string()) -> {ok, term()} | {error, term()}.
+-spec verify(SecretKey::string(), Reference::string()) -> {ok, term()} | {error, term()}.
+
 
 
 initialize(SecretKey, Reference, Amount, Email) ->
-	Url = "https://api.paystack.co/transaction/initialize",
+	%%Url = "https://api.paystack.co/transaction/initialize",
 	AuthHeader = {"Authorization", "Bearer " ++ SecretKey},
 	ApplicationTypeHeader = "application/json",
 	BodyMap = #{<<"reference">> => list_to_binary(Reference), <<"amount">> => list_to_binary(Amount), <<"email">> => list_to_binary(Email)},
 	BodyStr = binary_to_list(jsx:encode(BodyMap)),
-	Response = httpc:request(post, {Url, [AuthHeader], ApplicationTypeHeader, BodyStr}, [],[]),
+	Response = httpc:request(post, {?InitUrl, [AuthHeader], ApplicationTypeHeader, BodyStr}, [],[]),
+	case Response of
+		{ok, Result} ->
+			case Result of
+				{{_HttpVersion, 200, _Reason}, _Header, Body} ->
+					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
+				Error ->
+					{error, Error}
+			end;
+		{error, Reason} ->
+			{error, Reason}
+	end.
+
+verify(SecretKey, Reference) ->
+	Url = ?VerifyUrl ++ "/" ++ Reference,
+	AuthHeader = {"Authorization", "Bearer " ++ SecretKey},
+	Response = httpc:request(get, {Url, [AuthHeader]}, [],[]),
 	case Response of
 		{ok, Result} ->
 			case Result of
