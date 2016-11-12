af711bc6a160fcf2ed8463adafb95b8bd2d8883a
Abimbola Adegun <abimbola.adegun@gmail.com>
2016-11-12 12:26:24 +0100

    refactored, updated specs and added list_transactions

--- src/erlpaystack.erl
+++ src/erlpaystack.erl
@@ -1,32 +1,51 @@
 -module (erlpaystack).
 
--export ([initialize/4, verify/2, fetch_transaction/2]).
+-export ([initialize/4, charge_token/3, verify/2, fetch_transaction/2, charge_authorization/5, list_transactions/2]).
 -define (InitUrl, "https://api.paystack.co/transaction/initialize").
 -define (VerifyUrl, "https://api.paystack.co/transaction/verify").
 -define (FetchTransUrl, "https://api.paystack.co/transaction").
+-define (ChargeTokenUrl, "https://api.paystack.co/transaction/charge_token").
+-define (ChargeAuthUrl, "https://api.paystack.co/transaction/charge_authorization").
+-define (ListTransUrl, "https://api.paystack.co/transaction").
 
 -type apioption()::{string(), string()|integer()|boolean()|map()}.
+-type queryapioption()::{string(), string()|integer()}.
 -type apioptions()::[apioption()]|[].
+-type queryapioptions()::[queryapioption()]|[].
 -spec initialize(SecretKey::string(), Amount::integer(), Email::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 -spec verify(SecretKey::string(), Reference::string()) -> {ok, map()} | {error, term()}.
 -spec fetch_transaction(SecretKey::string(), Id::integer()) -> {ok, map()} | {error, term()}.
-
-
+-spec charge_token(SecretKey::string(), Token::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
+-spec charge_authorization(SecretKey::string(), AuthorizationCode::string(), Amount::integer(), Email::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
+-spec list_transactions(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
 
 initialize(SecretKey, Amount, Email, Options) ->
 	Url = ?InitUrl,
 	BodyMap = #{<<"amount">> => Amount, <<"email">> => list_to_binary(Email)},
-	CompleteBodyMap = utils:generateMap(BodyMap, Options),
-	BodyStr = binary_to_list(jsx:encode(CompleteBodyMap)),
-	httpc_wrapper:postUrl(SecretKey, Url, BodyStr).	
+	utils:post(SecretKey, Url, BodyMap, Options).
 
 verify(SecretKey, Reference) ->
 	Url = ?VerifyUrl ++ "/" ++ Reference,
-	httpc_wrapper:getUrl(SecretKey, Url).
+	utils:get(SecretKey, Url, []).
 
 fetch_transaction(SecretKey, Id) ->
 	Url = ?FetchTransUrl ++ "/" ++ integer_to_list(Id),
-	httpc_wrapper:getUrl(SecretKey, Url).
+	utils:get(SecretKey, Url, []). 
+
+charge_token(SecretKey, Token, Options) ->
+	Url = ?ChargeTokenUrl,
+	BodyMap = #{<<"token">> => list_to_binary(Token)},
+	utils:post(SecretKey, Url, BodyMap, Options).
+
+charge_authorization(SecretKey, AuthorizationCode, Amount, Email, Options) ->
+	Url = ?ChargeAuthUrl,
+	BodyMap = #{<<"authorization_code">> => AuthorizationCode, <<"amount">> => Amount, <<"email">> => list_to_binary(Email)},
+	utils:post(SecretKey, Url, BodyMap, Options).
+
+list_transactions(SecretKey, QueryOptions) ->
+	Url = ?ListTransUrl,
+	utils:get(SecretKey, Url, QueryOptions). 
+
 
 
 	
--- src/utils.erl
+++ src/utils.erl
@@ -1,7 +1,8 @@
 -module (utils).
--export ([generateMap/2]).
+-export ([generateMap/2, post/4, get/3, generateQueryStr/2]).
 
 generateMap(InitMap, []) -> InitMap;
+
 generateMap(InitMap, OptList) ->
 	[Opt|RemList] = OptList,
 	{Key, Value} = Opt,
@@ -15,3 +16,41 @@ generateMap(InitMap, OptList) ->
 	end,
 	UpdatedMap = InitMap#{MKey => MValue},
 	generateMap(UpdatedMap, RemList).
+
+post(SecretKey, Url, BodyMap, Options) ->
+	CompleteBodyMap = utils:generateMap(BodyMap, Options),
+	BodyStr = binary_to_list(jsx:encode(CompleteBodyMap)),
+	httpc_wrapper:postUrl(SecretKey, Url, BodyStr).	
+
+get(SecretKey, Url, Options) ->
+	QueryUrl = Url ++ utils:generateQueryStr([], Options),
+	httpc_wrapper:getUrl(SecretKey, QueryUrl).
+
+generateQueryStr(InitStr,[]) ->
+	InitStr;
+
+generateQueryStr([], OptList) ->
+	[Opt|RemList] = OptList,
+	{Key, Value} = Opt,
+	IntValueBool = is_integer(Value),
+	if
+		IntValueBool ->
+			NValue = integer_to_list(Value);
+		true ->
+			NValue = Value
+	end,
+	UpdatedStr = "?" ++ Key ++ "=" ++ NValue,
+	generateQueryStr(UpdatedStr, RemList);
+
+generateQueryStr(InitStr,OptList) ->
+	[Opt|RemList] = OptList,
+	{Key, Value} = Opt,
+	IntValueBool = is_integer(Value),
+	if
+		IntValueBool ->
+			NValue = integer_to_list(Value);
+		true ->
+			NValue = Value
+	end,
+	UpdatedStr = InitStr ++ "&" ++ Key ++ "=" ++ NValue,
+	generateQueryStr(UpdatedStr, RemList).
