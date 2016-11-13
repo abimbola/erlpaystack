32bc80b8512cca16a18ed0b2b16abd41f4e97ab9
Abimbola Adegun <abimbola.adegun@gmail.com>
2016-11-13 16:59:27 +0100

    Updated HTTP response handling. Refactored specs.

--- src/erlpaystack.erl
+++ src/erlpaystack.erl
@@ -18,10 +18,9 @@
 
 -define (InitUrl, "https://api.paystack.co/transaction/initialize").
 -define (VerifyUrl, "https://api.paystack.co/transaction/verify").
--define (FetchTransUrl, "https://api.paystack.co/transaction").
+-define (TransUrl, "https://api.paystack.co/transaction").
 -define (ChargeTokenUrl, "https://api.paystack.co/transaction/charge_token").
 -define (ChargeAuthUrl, "https://api.paystack.co/transaction/charge_authorization").
--define (ListTransUrl, "https://api.paystack.co/transaction").
 -define (ViewTransTimeLineUrl, "https://api.paystack.co/transaction/timeline").
 -define (TransactionTotalsUrl, "https://api.paystack.co/transaction/totals").
 -define (ExportTransactionsUrl, "https://api.paystack.co/transaction/export").
@@ -49,107 +48,93 @@
 -type queryapioption()::{string(), string()|integer()|boolean()}.
 -type apioptions()::[apioption()]|[].
 -type queryapioptions()::[queryapioption()]|[].
+
+%%Transactions API
+
 -spec initialize(SecretKey::string(), Amount::integer(), Email::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec verify(SecretKey::string(), Reference::string()) -> {ok, map()} | {error, term()}.
--spec fetch_transaction(SecretKey::string(), Id::integer()) -> {ok, map()} | {error, term()}.
--spec charge_token(SecretKey::string(), Token::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec charge_authorization(SecretKey::string(), AuthorizationCode::string(), Amount::integer(), Email::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec list_transactions(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
--spec view_transaction_timeline(SecretKey::string(), Reference::string()) -> {ok, map()} | {error, term()}.
--spec transaction_totals(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
--spec export_transactions(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
-
--spec create_customer(SecretKey::string(), Email::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec list_customers(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
--spec fetch_customer(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
--spec white_black_list_customer(SecretKey::string(), Customer::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec update_customer(SecretKey::string(), Customer::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
-
--spec create_plan(SecretKey::string(), Name::string(), Amount::integer(), Interval::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec list_plans(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
--spec fetch_plan(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
--spec update_plan(SecretKey::string(), Id::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
-
--spec create_subscription(SecretKey::string(), Customer::string(), Plan::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec list_subscriptions(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
--spec disable_subscription(SecretKey::string(), Code::string(), Token::string()) -> {ok, map()} | {error, term()}.
--spec enable_subscription(SecretKey::string(), Code::string(), Token::string()) -> {ok, map()} | {error, term()}.
--spec fetch_subscription(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
-
--spec create_page(SecretKey::string(), Name::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec list_pages(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
--spec fetch_page(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
--spec update_page(SecretKey::string() ,Id::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec check_slug_availability(SecretKey::string(), Slug::string()) ->  {ok, map()} | {error, term()}.
-
--spec create_subaccount(SecretKey::string(), BusinessName::string(), SettlementBank::string(), AccountNumber::string(), PercentageCharge::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec list_subaccounts(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
--spec fetch_subaccount(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
--spec update_subaccount(SecretKey::string() ,Id::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
--spec list_banks(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
-
--spec fetch_settlements(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
-
--spec resolve_card_bin(SecretKey::string(), Bin::string()) -> {ok, map()} | {error, term()}.
 
 initialize(SecretKey, Amount, Email, Options) ->
 	Url = ?InitUrl,
 	BodyMap = #{<<"amount">> => Amount, <<"email">> => list_to_binary(Email)},
 	utils:post(SecretKey, Url, BodyMap, Options).
 
+-spec verify(SecretKey::string(), Reference::string()) -> {ok, map()} | {error, term()}.
+
 verify(SecretKey, Reference) ->
 	Url = ?VerifyUrl ++ "/" ++ Reference,
 	utils:get(SecretKey, Url, []).
 
+-spec fetch_transaction(SecretKey::string(), Id::integer()) -> {ok, map()} | {error, term()}.
+
 fetch_transaction(SecretKey, Id) ->
-	Url = ?FetchTransUrl ++ "/" ++ integer_to_list(Id),
+	Url = ?TransUrl ++ "/" ++ integer_to_list(Id),
 	utils:get(SecretKey, Url, []). 
+
+-spec charge_token(SecretKey::string(), Token::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 
 charge_token(SecretKey, Token, Options) ->
 	Url = ?ChargeTokenUrl,
 	BodyMap = #{<<"token">> => list_to_binary(Token)},
 	utils:post(SecretKey, Url, BodyMap, Options).
 
+-spec charge_authorization(SecretKey::string(), AuthorizationCode::string(), Amount::integer(), Email::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
+
 charge_authorization(SecretKey, AuthorizationCode, Amount, Email, Options) ->
 	Url = ?ChargeAuthUrl,
 	BodyMap = #{<<"authorization_code">> => AuthorizationCode, <<"amount">> => Amount, <<"email">> => list_to_binary(Email)},
 	utils:post(SecretKey, Url, BodyMap, Options).
 
+-spec list_transactions(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
+
 list_transactions(SecretKey, QueryOptions) ->
-	Url = ?ListTransUrl,
+	Url = ?TransUrl,
 	utils:get(SecretKey, Url, QueryOptions). 
+
+-spec view_transaction_timeline(SecretKey::string(), Reference::string()) -> {ok, map()} | {error, term()}.
 
 view_transaction_timeline(SecretKey, Reference) ->
 	Url = ?ViewTransTimeLineUrl ++ "/" ++ Reference,
 	utils:get(SecretKey, Url, []).
 
+-spec transaction_totals(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
+
 transaction_totals(SecretKey, QueryOptions) ->
 	Url = ?TransactionTotalsUrl,
 	utils:get(SecretKey, Url, QueryOptions). 
 
+-spec export_transactions(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
 export_transactions(SecretKey, QueryOptions) ->
 	Url = ?ExportTransactionsUrl,
 	utils:get(SecretKey, Url, QueryOptions). 
 
 %%Customers API
+-spec create_customer(SecretKey::string(), Email::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 
 create_customer(SecretKey, Email, Options) ->
 	Url = ?CustomerUrl,
 	BodyMap = #{<<"email">> => list_to_binary(Email)},
 	utils:post(SecretKey, Url, BodyMap, Options).
 
+-spec list_customers(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
+
 list_customers(SecretKey, QueryOptions) ->
 	Url = ?CustomerUrl,
 	utils:get(SecretKey, Url, QueryOptions). 
-	
+
+-spec fetch_customer(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
+
 fetch_customer(SecretKey, Id) ->
 	Url = ?CustomerUrl ++ "/" ++ Id,
 	utils:get(SecretKey, Url, []).
+
+-spec white_black_list_customer(SecretKey::string(), Customer::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 
 white_black_list_customer(SecretKey, Customer, Options) ->
 	Url = ?WhiteBlackCustomerUrl,
 	BodyMap = #{<<"customer">> => list_to_binary(Customer)},
 	utils:post(SecretKey, Url, BodyMap, Options).
+
+-spec update_customer(SecretKey::string(), Customer::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.	
 
 update_customer(SecretKey ,Customer, Options) ->
 	Url = ?CustomerUrl ++ "/" ++ Customer,
@@ -157,105 +142,140 @@ update_customer(SecretKey ,Customer, Options) ->
 	utils:put(SecretKey, Url, BodyMap, Options).
 
 %%Plans API
+-spec create_plan(SecretKey::string(), Name::string(), Amount::integer(), Interval::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 
 create_plan(SecretKey, Name, Amount, Interval, Options) ->
 	Url = ?PlanUrl,
 	BodyMap = #{<<"name">> => list_to_binary(Name), <<"amount">> => Amount, <<"interval">> => list_to_binary(Interval)},
 	utils:post(SecretKey, Url, BodyMap, Options).
 
+-spec list_plans(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
+
 list_plans(SecretKey, QueryOptions) ->
 	Url = ?PlanUrl,
 	utils:get(SecretKey, Url, QueryOptions). 
+
+-spec fetch_plan(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
 
 fetch_plan(SecretKey, Id) ->
 	Url = ?PlanUrl ++ "/" ++ Id,
 	utils:get(SecretKey, Url, []).
 
+-spec update_plan(SecretKey::string(), Id::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 update_plan(SecretKey ,Id, Options) ->
 	Url = ?PlanUrl ++ "/" ++ Id,
 	BodyMap = #{},
 	utils:put(SecretKey, Url, BodyMap, Options).
 
 %%Subscriptions API
+-spec create_subscription(SecretKey::string(), Customer::string(), Plan::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 
 create_subscription(SecretKey, Customer, Plan, Options) ->
 	Url = ?SubscriptionUrl,
 	BodyMap = #{<<"customer">> => list_to_binary(Customer), <<"plan">> => list_to_binary(Plan)},
 	utils:post(SecretKey, Url, BodyMap, Options).
 
+-spec list_subscriptions(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
+
 list_subscriptions(SecretKey, QueryOptions) ->
 	Url = ?SubscriptionUrl,
 	utils:get(SecretKey, Url, QueryOptions). 
+
+-spec disable_subscription(SecretKey::string(), Code::string(), Token::string()) -> {ok, map()} | {error, term()}.
 
 disable_subscription(SecretKey, Code, Token) ->
 	Url = ?SubscriptionUrl ++ "/" ++ "disable",
 	BodyMap = #{<<"code">> => list_to_binary(Code), <<"token">> => list_to_binary(Token)},
 	utils:post(SecretKey, Url, BodyMap, []).
 
+-spec enable_subscription(SecretKey::string(), Code::string(), Token::string()) -> {ok, map()} | {error, term()}.
+
 enable_subscription(SecretKey, Code, Token) ->
 	Url = ?SubscriptionUrl ++ "/" ++ "enable",
 	BodyMap = #{<<"code">> => list_to_binary(Code), <<"token">> => list_to_binary(Token)},
 	utils:post(SecretKey, Url, BodyMap, []).
+
+-spec fetch_subscription(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
 
 fetch_subscription(SecretKey, Id) ->
 	Url = ?SubscriptionUrl ++ "/" ++ Id,
 	utils:get(SecretKey, Url, []).
 
 %% Pages API
+-spec create_page(SecretKey::string(), Name::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 
 create_page(SecretKey, Name, Options) ->
 	Url = ?PageUrl,
 	BodyMap = #{<<"name">> => list_to_binary(Name)},
 	utils:post(SecretKey, Url, BodyMap, Options).
 
+-spec list_pages(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
+
 list_pages(SecretKey, QueryOptions) ->
 	Url = ?PageUrl,
 	utils:get(SecretKey, Url, QueryOptions). 
 
+-spec fetch_page(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
+
 fetch_page(SecretKey, Id) ->
 	Url = ?PageUrl ++ "/" ++ Id,
 	utils:get(SecretKey, Url, []).
+
+-spec update_page(SecretKey::string() ,Id::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 
 update_page(SecretKey ,Id, Options) ->
 	Url = ?PageUrl ++ "/" ++ Id,
 	BodyMap = #{},
 	utils:put(SecretKey, Url, BodyMap, Options).
 
+-spec check_slug_availability(SecretKey::string(), Slug::string()) ->  {ok, map()} | {error, term()}.
+
 check_slug_availability(SecretKey, Slug) ->
 	Url = ?PageUrl ++ "/" ++ "check_slug_availability" ++ "/" ++  Slug,
 	utils:get(SecretKey, Url, []).
 
 %%Subaccounts API
+-spec create_subaccount(SecretKey::string(), BusinessName::string(), SettlementBank::string(), AccountNumber::string(), PercentageCharge::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 
 create_subaccount(SecretKey, BusinessName, SettlementBank, AccountNumber, PercentageCharge, Options) ->
 	Url = ?SubaccountUrl,
 	BodyMap = #{<<"business_name">> => list_to_binary(BusinessName), <<"settlement_bank">> => SettlementBank, <<"account_number">> => list_to_binary(AccountNumber), <<"percentage_charge">> => list_to_binary(PercentageCharge)},
 	utils:post(SecretKey, Url, BodyMap, Options).
 
+-spec list_subaccounts(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
+
 list_subaccounts(SecretKey, QueryOptions) ->
 	Url = ?SubaccountUrl,
 	utils:get(SecretKey, Url, QueryOptions). 
 
+-spec fetch_subaccount(SecretKey::string(), Id::string()) -> {ok, map()} | {error, term()}.
+
 fetch_subaccount(SecretKey, Id) ->
 	Url = ?SubaccountUrl ++ "/" ++ Id,
 	utils:get(SecretKey, Url, []).
+
+-spec update_subaccount(SecretKey::string() ,Id::string(), Options::apioptions()) -> {ok, map()} | {error, term()}.
 
 update_subaccount(SecretKey ,Id, Options) ->
 	Url = ?SubaccountUrl ++ "/" ++ Id,
 	BodyMap = #{},
 	utils:put(SecretKey, Url, BodyMap, Options).
 
+-spec list_banks(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
 list_banks(SecretKey, QueryOptions) ->
 	Url = ?BankUrl,
 	utils:get(SecretKey, Url, QueryOptions). 
 
+
 %%Settlements API
+-spec fetch_settlements(SecretKey::string(), QueryOptions::queryapioptions()) -> {ok, map()} | {error, term()}.
 
 fetch_settlements(SecretKey, QueryOptions) ->
 	Url = ?SettlementUrl,
 	utils:get(SecretKey, Url, QueryOptions).
 
 %% Miscellaneous API
+-spec resolve_card_bin(SecretKey::string(), Bin::string()) -> {ok, map()} | {error, term()}.
 
 resolve_card_bin(SecretKey, Bin) ->
 	Url = ?BinUrl ++ "/" ++ Bin,
--- src/httpc_wrapper.erl
+++ src/httpc_wrapper.erl
@@ -1,6 +1,8 @@
 -module (httpc_wrapper).
 -export ([getUrl/2, postUrl/3, putUrl/3]).
 
+-spec getUrl(SecretKey::string(), Url::string()) -> {ok, map()} | {error, term()}.
+
 getUrl(SecretKey, Url) ->
 	AuthHeader = {"Authorization", "Bearer " ++ SecretKey},
 	Response = httpc:request(get, {Url, [AuthHeader]}, [],[]),
@@ -9,12 +11,24 @@ getUrl(SecretKey, Url) ->
 			case Result of
 				{{_HttpVersion, 200, _Reason}, _Header, Body} ->
 					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
+				{{_HttpVersion, 201, _Reason}, _Header, Body} ->
+					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
+				{{_HttpVersion, StatusCode, Reason}, _Header, Body} ->
+					{error, {StatusCode, Reason, Body}};	
+				{200, Body} ->
+					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
+				{201, Body} ->
+					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
+				{StatusCode, Body} ->
+					{error, {StatusCode, Body}};
 				Error ->
 					{error, Error}
 			end;
 		{error, Reason} ->
 			{error, Reason}
 	end.
+
+-spec postUrl(SecretKey::string(), Url::string(), BodyStr::string()) -> {ok, map()} | {error, term()}.
 
 postUrl(SecretKey, Url, BodyStr) ->
 	AuthHeader = {"Authorization", "Bearer " ++ SecretKey},
@@ -29,6 +43,10 @@ postUrl(SecretKey, Url, BodyStr) ->
 					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
 				{{_HttpVersion, StatusCode, Reason}, _Header, Body} ->
 					{error, {StatusCode, Reason, Body}};	
+				{200, Body} ->
+					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
+				{201, Body} ->
+					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
 				{StatusCode, Body} ->
 					{error, {StatusCode, Body}};
 				Error ->
@@ -37,6 +55,8 @@ postUrl(SecretKey, Url, BodyStr) ->
 		{error, Reason} ->
 			{error, Reason}
 	end.
+
+-spec putUrl(SecretKey::string(), Url::string(), BodyStr::string()) -> {ok, map()} | {error, term()}.
 
 putUrl(SecretKey, Url, BodyStr) ->
 	AuthHeader = {"Authorization", "Bearer " ++ SecretKey},
@@ -51,6 +71,10 @@ putUrl(SecretKey, Url, BodyStr) ->
 					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
 				{{_HttpVersion, StatusCode, Reason}, _Header, Body} ->
 					{error, {StatusCode, Reason, Body}};	
+				{200, Body} ->
+					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
+				{201, Body} ->
+					{ok, jsx:decode(list_to_binary(Body), [return_maps])};
 				{StatusCode, Body} ->
 					{error, {StatusCode, Body}};
 				Error ->
